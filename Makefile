aeafcdbad7b641b44e0b9e56eeb82f15fd68096a
trguhq <93248917+trguhq@users.noreply.github.com>
2024-07-03 09:55:59 -0400

    iris gl and phigs driver headers

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ LDFLAGS = -lm -lGL -lGLU -lglut
 DRV_SRC = drv_ogl.c
 #DRV_SRC = drv_pgc.c
 #DRV_SRC = drv_xgl.c
+#DRV_SRC = drv_igl.c
+#DRV_SRC = drv_phg.c
 
 SOURCES = drv.c $(DRV_SRC) err.c globe.c main.c
 OBJECTS = $(SOURCES:.c=.o)
---
+++ drv_phg.h
@@ -0,0 +1,37 @@
+// testglobe - display 3d globe to test 3d hardware
+// drv_phg.h - PHIGS driver
+//
+// Copyright (C) 2024 trguhq
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef drv_phg_h
+#define drv_phg_h
+
+#ifndef DRV
+#define DRV
+#endif
+
+#define DRV_NAME "PHIGS"
+
+#define DRV_WIN_X 100
+#define DRV_WIN_Y 100
+#define DRV_WIN_WIDTH 640
+#define DRV_WIN_HEIGHT 480
+#define DRV_COLORMAPPED FALSE
+#define DRV_DEPTH 24
+#define DRV_COLORMAPDEPTH 24
+#define Color Color_RGB
+
+#endif /* drv_phg_h */
