f5103063024bc01faed3c6d23a1626c29b85a2f2
trguhq <93248917+trguhq@users.noreply.github.com>
2024-07-04 17:02:31 -0400

    Update drv_ogl.c
    include stdlib.h for exit()

--- drv_ogl.c
+++ drv_ogl.c
@@ -16,6 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#include <stdlib.h>
 #include "testglob.h"
 #include "glob_ext.h"
 #include "drv_ext.h"
